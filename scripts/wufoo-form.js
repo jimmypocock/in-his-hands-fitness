c70a68f9f71134f18c4ccbcccbca165f696e07d9
Jimmy Pocock <jimmypocock@yahoo.com>
2015-05-22 23:58:30 -0500

    It was broke cuz i removed email before it submitted.. Not no more.

--- scripts/wufoo-form.js
+++ scripts/wufoo-form.js
@@ -6,16 +6,12 @@ $(document).on('ready', function(){
     var $email = $('.wufoo input[type="email"]'),
         $submit = $('.wufoo input[type="submit"]'),
         $form = $('form.wufoo'),
-        $errors = $('.wufoo .errors');
+        $messages = $('.wufoo .messages');
 
     if ($email.val() === ""){
       e.preventDefault();
-      $errors.append('<label>An email address is required.</label>');
+      $messages.append('<label>An email address is required.</label>');
       return false;
-    } else {
-      $email.remove();
-      $submit.remove();
-      $('.wufoo .row').append('<h4>Thank you for signing up. Please check your email for our response within 24 hours.</h4>');
     }
   });
 });
