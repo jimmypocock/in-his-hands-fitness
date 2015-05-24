13e8f5acf41f39f5670cf280744d9599769de81c
Jimmy Pocock <jimmypocock@yahoo.com>
2015-05-24 11:18:24 -0500

    Fixing stuff.

--- app/scripts/full-width.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var resetDimensions = function() {
-  var winHeight = $(window).height()
-  $(".main.section").css("max-height", (winHeight - 150) + "px");
-  $(".main.section").css("height", (winHeight - 150) + "px")
-}
-
-$(window).on('resize', resetDimensions);
