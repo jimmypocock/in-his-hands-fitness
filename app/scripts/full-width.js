2f887f1175e002612b585ef1d1f6052fd18074d1
Jimmy Pocock <jimmypocock@yahoo.com>
2015-05-24 15:40:45 -0500

    Added the testimonials

--- app/scripts/full-width.js
+++ app/scripts/full-width.js
@@ -1,14 +1,18 @@
 'use strict';
 
 var resetDimensions = function() {
-  var winHeight = $(window).height(),
-      section = $('.top.section'),
-      boxHeight = $('.subscription.form').height(),
-      navHeight = 52,
-      paddingTop = (winHeight - boxHeight) / 2 - navHeight - 35;
-  section.css('max-height', (winHeight - navHeight) + 'px');
-  section.css('height', (winHeight - navHeight) + 'px');
-  section.css('padding-top', paddingTop);
+  // var winHeight = $(window).height(),
+  //     section = $('.top.section'),
+  //     boxHeight = $('.subscription.form').height(),
+  //     navHeight = 52,
+  //     paddingTop = (winHeight - boxHeight) / 2 - navHeight - 35;
+  // if ($(window).width() >= 768) {
+  //   if ((winHeight) > boxHeight + 52) {
+  //     section.css('max-height', (winHeight - navHeight) + 'px');
+  //     section.css('height', (winHeight - navHeight) + 'px');
+  //   }
+  //   section.css('padding-top', paddingTop);
+  // }
 }
 
 $(window).on('resize', resetDimensions);
